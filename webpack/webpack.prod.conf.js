5f4916f3a6b7551c0c17e8a6342d70b1310c9f19
Ice Lam <pinky.yy.lam@gmail.com>
2020-01-02 15:27:22 +0800

    Update project dependencies to solve possible vulnerabilities

--- webpack/webpack.prod.conf.js
+++ webpack/webpack.prod.conf.js
@@ -1,7 +1,6 @@
 const Webpack = require('webpack');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const autoprefixer = require('autoprefixer');
@@ -21,18 +20,6 @@ module.exports = merge(baseWebpackConfig, {
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new Webpack.optimize.ModuleConcatenationPlugin(),
-    new UglifyJSPlugin({
-      uglifyOptions: {
-        warnings: false,
-        compress: {
-          drop_debugger: true,
-          drop_console: true
-        },
-        ecma: 8
-      },
-      sourceMap: true,
-      parallel: true
-    }),
     new MiniCssExtractPlugin({
       filename: 'assets/css/[name].bundle.css'
     }),
@@ -51,7 +38,12 @@ module.exports = merge(baseWebpackConfig, {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            envName: 'production'
+          }
+        }]
       },
       {
         test: /\.s?css/i,
